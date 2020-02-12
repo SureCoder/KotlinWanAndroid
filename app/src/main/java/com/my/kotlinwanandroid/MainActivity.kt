cbaaac753c896499cc335f26cf5fe48707f5500c
LiShuo <lishuo940419@163.com>
2020-02-12 17:26:19 +0800

    依赖项

--- app/src/main/java/com/my/kotlinwanandroid/MainActivity.kt
+++ app/src/main/java/com/my/kotlinwanandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.my.kotlinwanandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
